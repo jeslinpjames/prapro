e8fa419132e219c1bd910f5388d9c8713c405bb0
jeslinpjames <jeslinpjames007@gmail.com>
2024-03-06 14:15:33 +0530

    started task 6

---
+++ task_6/task_6_p1.py
@@ -0,0 +1,12 @@
+# Task 6
+
+# Part 1:
+# Optimize download speeds in Downloader class by using threads.
+# Monitor CPU usage and time taken.
+
+# References:
+# concurrent.futures.ThreadPoolExecutor
+
+# Part 2:
+# Pick an open-source software on GitHub
+# Write a script that showcases the basic usage of the software
