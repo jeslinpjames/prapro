6589cce115e1874ccecc4fb55080fdd253adf4d0
jeslinpjames <jeslinpjames007@gmail.com>
2024-03-22 22:13:00 +0530

    started task 11

---
+++ task_11/task_11.py
@@ -0,0 +1,12 @@
+# Task 11
+
+# Use multiprocessing to fill random values in a large numpy array.
+
+# References:
+# multiprocessing.shared_memory.SharedMemory
+# concurrent.futures.ProcessPoolExecutor
+
+# Read about:
+# Python Global Interpreter Lock (GIL)
+# Multiprocessing start methods: spawn, fork, forkserver
+
