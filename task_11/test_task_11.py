a19ac6e9744a24235189d0680a0750aa4c7039b1
jeslinpjames <jeslinpjames007@gmail.com>
2024-03-23 15:43:22 +0530

    reformatted code

--- task_11/test_task_11.py
+++ task_11/test_task_11.py
@@ -3,6 +3,7 @@ import numpy as np
 import pytest
 from multiprocessing import shared_memory
 
+
 def test_runProcesses():
     sm_name = "test_shared_memory"
     size = 1000
@@ -14,11 +15,14 @@ def test_runProcesses():
     assert arr.dtype == np.int16
     assert np.all(arr >= 0) and np.all(arr < 100)
 
+
 def test_fill_arr():
     sm_name = "test_shared_memory"
     size = 10
     shape = (size,)
-    shared_arr = shared_memory.SharedMemory(name=sm_name, size=shape[0] * np.dtype(np.int16).itemsize, create=True)
+    shared_arr = shared_memory.SharedMemory(
+        name=sm_name, size=shape[0] * np.dtype(np.int16).itemsize, create=True
+    )
     arr = np.ndarray(shape, dtype=np.int16, buffer=shared_arr.buf)
     arr.fill(0)
 
