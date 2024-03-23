c020807481ee756d46f4a4a11d2c41e45c1140dd
jeslinpjames <jeslinpjames007@gmail.com>
2024-03-23 10:40:51 +0530

    fill array function added

--- task_11/task_11.py
+++ task_11/task_11.py
@@ -10,3 +10,15 @@
 # Python Global Interpreter Lock (GIL)
 # Multiprocessing start methods: spawn, fork, forkserver
 
+import multiprocessing.shared_memory.SharedMemory 
+import concurrent.futures.ProcessPoolExecutor
+import numpy as np
+
+
+def fill_arr(slice):
+    start = slice.start
+    end = slice.stop
+    len = end - start
+    arr[start:end] =np.random.randint(0,100,len)
+
+
